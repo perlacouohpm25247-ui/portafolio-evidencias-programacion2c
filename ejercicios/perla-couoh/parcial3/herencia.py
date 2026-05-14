8a334dd7251cb307e8a8dde3eda1e3cb0c98151d
Perla Couoh <perla.couoh_pm25247@cecyte.edu.mx>
2026-05-14 11:48:14 -0600

    Se entrega 3 ejercicios realizados del parcial3

---
+++ ejercicios/perla-couoh/parcial3/herencia.py
@@ -0,0 +1,19 @@
+class Ave:
+    def __init__(self, color = "Verde"):
+        self.color = color
+
+    def volar(self):
+        print("Puedo volar")
+
+class Canario(Ave):
+    def __init__(self, color, nombre):
+        super().__init__(color)
+        self.nombre = nombre
+
+    def informacion(self):
+        pass
+
+Canario = Canario("Amarillo", "Fulanito")
+print(Canario.color)
+Canario.volar()
+print(Canario.color)
